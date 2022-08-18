c653b89a13e148b9c7c773b8ecd09d1938c5c4b1
2JISUN <snm03097@gmail.com>
2022-08-18 11:36:14 +0900

    feat: Print fizzbuzz
    condition: i is times of 15
    adjust numrange 1~16 to 1~20

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,5 +1,7 @@
-for i in range(1, 16):
-    fi i%3==0:
+for j in range(1, 20+1):
+    if i%15==0:
+        print('fizzbuzz')
+    elif i%3==0:
         print('fizz')
     else:
-        print(i)
+        print(j)
