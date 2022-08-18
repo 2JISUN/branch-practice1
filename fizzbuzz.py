89f1bd4ff3e3e6e661a0ccbde9b405b6410607da
2JISUN <snm03097@gmail.com>
2022-08-18 11:41:24 +0900

    feat: Print buzz
    Condition: i is times of 5

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,5 +1,7 @@
-for i in range(1, 16):
-    fi i%3==0:
-        print('fizz')
+for i in range(1, 19+1):
+    if i%3==0:
+        print('FIZZ')
+    elif i%5==0:
+        print('BUZZ')
     else:
         print(i)
